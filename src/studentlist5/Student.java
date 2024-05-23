4acd6f4ab409dd416c0c94a541e0bdab4900a292
defranjo <joe.defranco@sheridancollege.ca>
2024-05-23 11:39:53 -0400

    Sixth comment

--- src/studentlist5/Student.java
+++ src/studentlist5/Student.java
@@ -9,5 +9,33 @@ package studentlist5;
  * @author joede
  */
 public class Student {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    private String name;
+    private String studentID;
+    private String address;
     
 }
